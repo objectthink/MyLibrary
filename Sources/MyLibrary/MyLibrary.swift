792e0b1b9120f951de7777294b5c1187441a5560
stephen eshelman, objectthink <stephen.n.eshelman@objectthink.com>
2022-02-27 12:04:58 -0500

    add ants connect test

--- Sources/MyLibrary/MyLibrary.swift
+++ Sources/MyLibrary/MyLibrary.swift
@@ -7,6 +7,17 @@ public struct MyLibrary {
    }
    
    public func connectTest(){
+      var client: NatsClient!
       
+      // register a new client
+      client = NatsClient("http://192.168.86.31:4222")
+      
+      // listen to an event
+      client!.on(NatsEvent.connected) { _ in
+         print("Client connected")
+      }
+      
+      // try to connect to the server
+      try? client!.connect()
    }
 }
